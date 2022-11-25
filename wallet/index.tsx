99afd798a33bc9f11b4ec82fa5e2c28b3bae656d
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-11-25 16:08:43 +0100

    remove unused folder

--- wallet/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from "../packages/core/src/utils/WalletChainContext";
-export * from "./hooks";
-export * from "./soroban-react";
-export * from "../packages/core/src/utils/provideWalletChains";
-export * from "../packages/core/src/getDefaultConnectors";
