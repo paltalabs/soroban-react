3b99fc7d06d59ade91a15a0a628656f0343c97a2
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-11-25 14:24:16 +0100

    @soroban-react/types package

--- packages/types/src/index.tsx
+++ packages/types/src/index.tsx
@@ -29,3 +29,25 @@ export type ConnectorList = {
   groupName: string;
   connectors: Connector[]
 }[];
+
+
+export interface WalletChain {
+  id: string;
+  name?: string;
+  networkPassphrase: string;
+  iconBackground?: string;
+  iconUrl?: string | null;
+  // TODO: Use this to indicate which chains a dapp supports
+  unsupported?: boolean;
+};
+
+// Sourced from https://github.com/tmm/wagmi/blob/main/packages/core/src/constants/chains.ts
+// This is just so we can clearly see which of wagmi's first-class chains we provide metadata for
+export type ChainName =
+  | 'futurenet'
+  | 'public'
+  | 'testnet'
+  | 'sandbox'
+  | 'standalone';
+
+export type ChainMetadata = WalletChain;
