f2fab73575871f340f182689e164c4451b75ad42
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-11-30 13:49:27 +0100

    @soroban-react/connect-button

---
+++ packages/connect-button/src/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useSorobanReact } from '@soroban-react/core'
+//import styles from './style.module.css'
+
+export interface ConnectButtonProps {
+  label: string
+  isHigher?: boolean
+}
+
+const buttonStyle: any = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "16px 12px",
+  gap: "8px",
+  background: "#1a1523",
+  borderRadius: "8px",
+  border: "0",
+  height: "38px",
+  fontWeight: "600",
+  fontSize: "14px",
+  lineHeight: "22px",
+  color: "#ffffff",
+  cursor: "pointer",
+}
+
+
+export function ConnectButton({ label, isHigher }: ConnectButtonProps) {
+  const { connect } = useSorobanReact()
+  const openConnectModal = async () => {
+    await connect()
+  }
+
+  return (
+    <button
+      className={buttonStyle}
+      style={{ height: isHigher ? 50 : 38 }}
+      onClick={openConnectModal}
+    >
+      {label}
+    </button>
+  )
+}
