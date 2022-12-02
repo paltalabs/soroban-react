774ea9217a7b6ef1c7e954aebf326bc7201c52c5
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-02 11:27:59 +0100

    basic button

--- packages/connect-button/src/index.tsx
+++ packages/connect-button/src/index.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import {SorobanContextType} from '@soroban-react/core'
 
+
 export interface ConnectButtonProps {
   label: string;
   isHigher?: boolean;
   sorobanContext: SorobanContextType;
 }
-
-const buttonStyle: any = {
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "16px 12px",
-  gap: "8px",
-  background: "#1a1523",
-  borderRadius: "8px",
-  border: "0",
-  height: "38px",
-  fontWeight: "600",
-  fontSize: "14px",
-  lineHeight: "22px",
-  color: "#ffffff",
-  cursor: "pointer",
-}
-
 
 export function ConnectButton({ label, isHigher, sorobanContext }: ConnectButtonProps) {
   const {connect} = sorobanContext
@@ -34,7 +16,6 @@ export function ConnectButton({ label, isHigher, sorobanContext }: ConnectButton
 
   return (
     <button
-      className={buttonStyle}
       style={{ height: isHigher ? 50 : 38 }}
       onClick={openConnectModal}
     >
