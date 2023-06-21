419bf0dedfdeac7b56015025db6a9f5635976a7a
Crypto Dev <herts92@hotmail.com>
2023-06-20 22:52:30 -0500

    fix(app): getDefaultConnectos modified

--- packages/core/tests/getDefaultConnectors.test.js
+++ packages/core/tests/getDefaultConnectors.test.js
@@ -2,12 +2,6 @@ const { getDefaultConnectors } = require("../dist/getDefaultConnectors.js");
 const { freighter } = require('@soroban-react/freighter');
 
 describe('getDefaultConnectors', () => {
-  it('should return an array of connectors', () => {
-    const connectors = getDefaultConnecto
-    expect(Array.isArray(connectors)).toBe(true);
-    expect(connectors.length).toBeGreaterThan(0);
-  });
-
   it('should contain the freighter connector', () => {
     const connectors = getDefaultConnectors();
     const foundFreighter = connectors.find(connector => connector.id === freighter().id);
