5a0629f7ad7104b9c5e2b1612bca5911cf618e39
benjaminsalon <benjaminsalon.business@gmail.com>
2023-10-20 10:00:38 +0200

    updated with error throwing if soroban rpc not present - NOT TESTED YET

--- packages/core/src/SorobanReactProvider.tsx
+++ packages/core/src/SorobanReactProvider.tsx
@@ -73,10 +73,16 @@ export function SorobanReactProvider({
 
         let address = await mySorobanContext.activeConnector?.getPublicKey()
         //TODO: Fix how futurenet soroban rpc is handled, it should get it right from the freighter api, but for now this is the workaround
+        if (! networkDetails?.sorobanRpcUrl) {
+          const error = new Error(
+            'Soroban RPC URL is not set, please check your freighter wallet network configuration'
+          )
+          throw error
+        }
         let server =
           networkDetails &&
           new SorobanClient.Server(
-            networkDetails.sorobanRpcUrl ?? networkDetails.networkUrl,
+            networkDetails.sorobanRpcUrl,
             {
               allowHttp: networkDetails.networkUrl.startsWith('http://'),
             }
