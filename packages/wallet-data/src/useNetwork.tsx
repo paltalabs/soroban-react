f962bdb188450067bbf86441e2706805119ccb0e
benjaminsalon <benjaminsalon.business@gmail.com>
2023-11-07 23:24:34 +0100

    yarn build passing with code uncommented

--- packages/wallet-data/src/useNetwork.tsx
+++ packages/wallet-data/src/useNetwork.tsx
@@ -1,22 +1,22 @@
-// import { SorobanContextType } from '@soroban-react/core'
-// import { WalletChain } from '@soroban-react/types'
-// import React from 'react'
+import { SorobanContextType } from '@soroban-react/core'
+import { WalletChain } from '@soroban-react/types'
+import React from 'react'
 
-// import * as SorobanClient from 'soroban-client'
+import * as SorobanClient from 'soroban-client'
 
-// import { WalletChainByName } from './provideWalletChains'
+import { WalletChainByName } from './provideWalletChains'
 
-// export type NetworkConfig = {
-//   activeChain?: WalletChain
-//   server?: SorobanClient.Server
-//   chains: Array<WalletChain>
-// }
+export type NetworkConfig = {
+  activeChain?: WalletChain
+  server?: SorobanClient.Server
+  chains: Array<WalletChain>
+}
 
-// export function useNetwork(sorobanContext: SorobanContextType): NetworkConfig {
-//   const { activeChain, server } = sorobanContext
-//   return {
-//     activeChain,
-//     server,
-//     chains: Object.values(WalletChainByName),
-//   }
-// }
+export function useNetwork(sorobanContext: SorobanContextType): NetworkConfig {
+  const { activeChain, server } = sorobanContext
+  return {
+    activeChain,
+    server,
+    chains: Object.values(WalletChainByName),
+  }
+}
