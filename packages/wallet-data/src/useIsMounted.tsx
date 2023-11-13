aecafcf459742ac97f4cc4462393f80f659e7907
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-11-13 10:35:53 -0300

    uncomment useIsMounted

--- packages/wallet-data/src/useIsMounted.tsx
+++ packages/wallet-data/src/useIsMounted.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-// export function useIsMounted() {
-//   const reducer = () => true
-//   const [mounted, setMounted] = React.useReducer(reducer, false)
-//   React.useEffect(setMounted, [setMounted])
-//   return mounted
-// }
+export function useIsMounted() {
+  const reducer = () => true
+  const [mounted, setMounted] = React.useReducer(reducer, false)
+  React.useEffect(setMounted, [setMounted])
+  return mounted
+}
 
