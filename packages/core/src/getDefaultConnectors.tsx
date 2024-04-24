ac53933b0d92ea642e77efd4a89646db7a20db23
coderipper <hello@joaquinsoza.dev>
2024-04-24 16:40:27 +0200

    feat: added lobstr package to core

--- packages/core/src/getDefaultConnectors.tsx
+++ packages/core/src/getDefaultConnectors.tsx
@@ -1,14 +1,15 @@
 import { freighter } from '@soroban-react/freighter'
+import { lobstr } from '@soroban-react/lobstr'
 import { Connector } from '@soroban-react/types'
 import { xbull } from '@soroban-react/xbull'
 
 /**
  * Retrieves default wallet connectors.
- * These connectors include for now 'freighter' and 'xbull'.
+ * These connectors include for now 'freighter', 'xbull' and 'lobstr'.
  * @returns An array of default connectors.
  */
 export const getDefaultConnectors = (): Connector[] => {
-  const list: Connector[] = [freighter(),xbull()]
+  const list: Connector[] = [freighter(),xbull(),lobstr()]
 
   return list
 }
